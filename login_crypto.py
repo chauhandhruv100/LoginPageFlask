2e63da43653f944eb9ccb55369113e181d534b2c
chauhandhruv100 <dhruvchauhan100@gmail.com>
2021-05-06 20:06:43 +0545

    Update login_crypto.py

--- login_crypto.py
+++ login_crypto.py
@@ -1,7 +1,6 @@
 from cryptography.fernet import Fernet
 from flask import Flask, render_template, url_for, request, session, redirect
 from flask_pymongo import PyMongo
-import bcrypt
 from pymongo import MongoClient
 import json
 
